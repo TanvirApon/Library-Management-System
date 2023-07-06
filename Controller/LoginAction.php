0e13a82d5eb16ccb4cfb0c2c393479213b76951e
tanvirhossainapon <tanvirhossainapon@gmail.com>
2023-07-06 16:47:39 +0600

    bug fixed

--- Controller/LoginAction.php
+++ Controller/LoginAction.php
@@ -16,31 +16,36 @@ if(isset($_POST['login_button'])) {
 	$_SESSION['email'] = $email; //Store email into session variable 
 	$password = $_POST['password']; //Get password
 
+    if($email === "admin.admin@gmail.com" && $password === "admin555")
+    {
+        header("Location:../View/admin/Dashboard.php");
+		exit();
+
+    }
+    else
+    {
+        $password = md5($_POST['password']); //Get password
 	// Check if the user is a customer
-	$query = "SELECT * FROM customer WHERE email='$email'";
-	$result = mysqli_query($conn, $query);
+	$check_database_query = mysqli_query($conn, "SELECT * FROM customer WHERE email='$email' AND password='$password'");
+	$check_login_query = mysqli_num_rows($check_database_query);
 
-	if(mysqli_num_rows($result) == 1) {
-		$row = mysqli_fetch_assoc($result);
-		$hashed_password = $row['password'];
-
-		if(password_verify($password, $hashed_password)) {
-			$username = $row['username'];
+	if($check_login_query == 1) {
+		$row = mysqli_fetch_array($check_database_query);
+		 $username = $row['username'];
 			$_SESSION['username'] = $username;
 			header("Location: ../View/customer/Dashboard.php");
 			exit();
 		}
-	}
 
-	// Check if the user is an admin
-	if($email === "admin.admin@gmail.com" && $password === "admin555") {
-		header("Location:../View/admin/Dashboard.php");
-		exit();
-	}
+	    }
 
-	// If neither customer nor admin, show error message
-	array_push($error_array, "Email or password was incorrect<br>");
-}
+        // If neither customer nor admin, show error message
+	  array_push($error_array, "Email or password was incorrect<br>");
+
+
+    }
+
+	
 
 
 
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+
+header("Location:View/login.php");
+
+
+?>
